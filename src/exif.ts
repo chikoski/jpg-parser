07fe4c94c90a95ffdb1f0d64c68fed473818252b
N.Shimizu <chikoski@gmail.com>
2019-04-12 07:13:42 +0900

    added mock code

--- src/exif.ts
+++ src/exif.ts
@@ -2,8 +2,12 @@ import { APPSegment } from "./segment";
 
 const identifier = Array.from("Exif").map(i => i.charCodeAt(0)).reduce((i, j) => i << 8 | j, 0);
 
-export class EXIF {
-  constructor(segment: APPSegment) {
-
-  }
+export function parse(segment: APPSegment) {
+  const data = segment.data;
+  const header = parseHeader(data);
+  const eixfIFD = parseIFD(data, header.size);
+  const gpsIFD = parseIFD(data, eixfIFD.nextIFDPointer);
+  const compatibilityIFD = parseIFD(data, gpsIFD.nextIFDPointer);
+  const firstIFD = parseIFD(data, compatibilityIFD.nextIFDPointer);
+  const thumbnail = new DataView(data.buffer, firstIFD.nextIFDPointer);
 }
