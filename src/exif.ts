ede7fa4e535356e4ac7a63ec92548e9d302797f1
Chiko Shimizu <chikoski@google.com>
2019-04-12 18:07:33 +0900

    implement tiff file format parser

--- src/exif.ts
+++ src/exif.ts
@@ -1,9 +1,28 @@
-import { APPSegment } from "./segment";
+import {APPSegment} from './segment';
+import * as Tiff from './tiff';
 
-const identifier = Array.from("Exif").map(i => i.charCodeAt(0)).reduce((i, j) => i << 8 | j, 0);
+const identifier = Array.from('Exif')
+                       .map(i => i.charCodeAt(0))
+                       .reduce((i, j) => i << 8 | j, 0);
+const TIFF_HEADER_STARTING_OFFSET = 12;
 
-export class EXIF {
-  constructor(segment: APPSegment) {
+export class EXIF {}
 
+
+function containsExifData(segment: APPSegment) {
+  const data = segment.data;
+  return segment.type === 1 && segment.size > 8 &&
+      data.getUint32(6) === identifier && data.getUint16(10) === 0;
+}
+
+
+export function parse(segment: APPSegment) {
+  if (!containsExifData(segment)) {
+    return null;
+  };
+  const data = segment.data;
+  const tiff = Tiff.parse(data, TIFF_HEADER_STARTING_OFFSET);
+  if (!tiff.isValid()) {
+    return null;
   }
 }
